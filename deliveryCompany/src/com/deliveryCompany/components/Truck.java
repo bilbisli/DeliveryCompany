f4dcc18557bbd4a4593af22b2fc54e182c55f66f
ofirgls <ofirgls5588@gmail.com>
2021-03-31 18:09:55 +0300

    Truck class completion

--- deliveryCompany/src/com/deliveryCompany/components/Truck.java
+++ deliveryCompany/src/com/deliveryCompany/components/Truck.java
@@ -1,19 +1,151 @@
 package com.deliveryCompany.components;
-
+import java.util.Random;
 import java.util.ArrayList;
 
-public class Truck {
+public abstract class Truck {
+	static private int nextId = 2000; 
 	private int truckID;
-	private String licensePlate;
-	private String truckModel;
+	final private String licensePlate;
+	final private String truckModel;
 	private boolean available;
 	private int timeLeft;
 	private ArrayList<Package> packages;
 	
-	public Truck() {}
+	public Truck() {
+		truckID = nextId++;	
+		Random rand = new Random();
+		truckModel = "M" + rand.nextInt(5);
+		available = true;
+		packages = new ArrayList<Package>();
+		timeLeft = 0;
+		licensePlate = pattern();
+	}
 	
 	public Truck(String licensePlate, String truckModel) {
 		this.licensePlate = licensePlate;
 		this.truckModel = truckModel;
+		available = true;
+		packages = new ArrayList<Package>();
+		truckID = nextId++;	
+		timeLeft = 0;
 	}
+	
+	
+	public int getnextId() {
+		return nextId;
+	}
+	
+	public int getTruckID() {
+		return truckID;
+	}
+
+	public void setTruckID(int truckID) {
+		this.truckID = truckID;
+	}
+
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+
+
+	public String getTruckModel() {
+		return truckModel;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public int getTimeLeft() {
+		return timeLeft;
+	}
+
+	public void setTimeLeft(int timeLeft) {
+		this.timeLeft = timeLeft;
+	}
+
+	public ArrayList<Package> getPackages() {
+		return packages;
+	}
+
+	public void setPackages(ArrayList<Package> packages) {
+		this.packages = packages;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (available ? 1231 : 1237);
+		result = prime * result + ((licensePlate == null) ? 0 : licensePlate.hashCode());
+		result = prime * result + ((packages == null) ? 0 : packages.hashCode());
+		result = prime * result + timeLeft;
+		result = prime * result + truckID;
+		result = prime * result + ((truckModel == null) ? 0 : truckModel.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Truck other = (Truck) obj;
+		if (available != other.available)
+			return false;
+		if (licensePlate == null) {
+			if (other.licensePlate != null)
+				return false;
+		} else if (!licensePlate.equals(other.licensePlate))
+			return false;
+		if (packages == null) {
+			if (other.packages != null)
+				return false;
+		} else if (!packages.equals(other.packages))
+			return false;
+		if (timeLeft != other.timeLeft)
+			return false;
+		if (truckID != other.truckID)
+			return false;
+		if (truckModel == null) {
+			if (other.truckModel != null)
+				return false;
+		} else if (!truckModel.equals(other.truckModel))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Truck [truckID=" + truckID + ", licensePlate=" + licensePlate + ", truckModel=" + truckModel
+				+ ", available=" + available + ", timeLeft=" + timeLeft + ", packages=" + packages + ", getTruckID()="
+				+ getTruckID() + ", getLicensePlate()=" + getLicensePlate() + ", getTruckModel()=" + getTruckModel()
+				+ ", isAvailable()=" + isAvailable() + ", getTimeLeft()=" + getTimeLeft() + ", getPackages()="
+				+ getPackages() + ", hashCode()=" + hashCode() + ", getClass()=" + getClass() + ", toString()="
+				+ super.toString() + "]";
+	}
+	
+	
+	public String pattern() {
+		int n = 10000000 + new Random().nextInt(100000000);
+		int temp[] = new int[8];
+		int i = 0;
+		while(n>0) {
+			temp[i] = n%10;
+			i++;
+			n = n/10;
+		}
+		return temp[0] + "" + temp[1] + "" + temp[2] + "-" + temp[3] + "" + temp[4] + "-" + temp[5] + "" + temp[6] + "" + temp[7];
+		
+	}
+
+		
 }
+
