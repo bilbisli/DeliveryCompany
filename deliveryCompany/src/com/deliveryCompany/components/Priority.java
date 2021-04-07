ddb11d1974daabd038b710c81c37e67d33a803da
Israel Avihail <bilbisli@gmail.com>
2021-04-07 23:51:49 +0300

    half of comments done

--- deliveryCompany/src/com/deliveryCompany/components/Priority.java
+++ deliveryCompany/src/com/deliveryCompany/components/Priority.java
@@ -1,16 +1,13 @@
 package com.deliveryCompany.components;
 
+/**
+ * This enum is for setting the priority of a certain package shipment
+ * @version	1.00 7 Apr 2021
+ * @author	Israel Avihail
+ * @see		Package
+ */
 public enum Priority {
-	/**
-	 * 
-	 */
 	LOW,
-	/**
-	 * 
-	 */
 	STANDARD,
-	/**
-	 * 
-	 */
 	HIGHT
 }
--- deliveryCompany/src/com/deliveryCompany/components/Status.java
+++ deliveryCompany/src/com/deliveryCompany/components/Status.java
@@ -1,40 +1,19 @@
 package com.deliveryCompany.components;
 
+/**
+ * This enum is for setting the status of a certain package
+ * @version	1.00 7 Apr 2021
+ * @author	Israel Avihail
+ * @see		Package
+ */
 public enum Status {
-	/**
-	 * 
-	 */
 	CREATION,
-	/**
-	 * 
-	 */
 	COLLECTION,
-	/**
-	 * 
-	 */
 	BRANCH_STORAGE,
-	/**
-	 * 
-	 */
 	HUB_TRANSPORT,
-	/**
-	 * 
-	 */
 	HUB_STORAGE,
-	/**
-	 * 
-	 */
 	BRANCH_TRANSPORT,
-	/**
-	 * 
-	 */
 	DELIVERY,
-	/**
-	 * 
-	 */
 	DISTRIBUTION,
-	/**
-	 * 
-	 */
 	DELIVERED
 }
